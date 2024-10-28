0325508262b3e984657521b46527b8dc58ca848f
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2024-10-28 20:28:53 +0100

    Change sign click handling

--- src/main/java/de/jdevr/pluto/listeners/InteractionEventListener.java
+++ src/main/java/de/jdevr/pluto/listeners/InteractionEventListener.java
@@ -1,22 +1,30 @@
 package de.jdevr.pluto.listeners;
 
-import de.jdevr.pluto.WorldUtils;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import de.jdevr.pluto.Pluto;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import java.io.IOException;
+
 public class InteractionEventListener implements Listener {
     @EventHandler
-    public void onInteract(PlayerInteractEvent event) {
+    public void onInteract(PlayerInteractEvent event) throws IOException {
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             if (event.getClickedBlock().getState() instanceof Sign) {
-                Sign s = (Sign) event.getClickedBlock().getState();
-                String message = String.join("\n", s.getLines());
-                if (message.startsWith("Change world:\n")) {
-                    event.setCancelled(true);
-                    WorldUtils.TeleportPlayerToWorld(event.getPlayer(), s.getLines()[1]);
+                JsonObject jsonObject;
+                for (JsonElement jsonElement : Pluto.signData.getAsArray().asList()) {
+                    jsonObject = jsonElement.getAsJsonObject();
+                    if ((event.getClickedBlock().getX() == jsonObject.get("x").getAsInt())
+                            && (event.getClickedBlock().getY() == jsonObject.get("y").getAsInt())
+                            && (event.getClickedBlock().getZ() == jsonObject.get("z").getAsInt())
+                    ) {
+                        event.getPlayer().performCommand(jsonObject.get("cmd").getAsString());
+                    }
                 }
             }
         }
